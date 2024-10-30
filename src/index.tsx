385901b6e7bfa7244aad70ac973fab1697b9965c
Jason Quaicoo <jayquake@gmail.com>
2024-10-30 12:28:05 +0000

    "Add deployment configuration for GitHub Pages with BrowserRouter"

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
-const rootElement = document.getElementById("root")!;
-const root = ReactDOM.createRoot(rootElement);
-
-root.render(
-  <React.StrictMode>
+ReactDOM.render(
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>,
+  document.getElementById("root")
 );
